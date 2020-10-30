6a93e4dcdf9aa1a6b1a2be6e4c7dab12f5b03f10
root <root@Obninsk.ews.local>
2020-10-30 13:46:51 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"frontier_silicon adapter settings": {
+		"en": "Adapter settings for frontier_silicon",
+		"de": "Adaptereinstellungen für frontier_silicon",
+		"ru": "Настройки адаптера для frontier_silicon",
+		"pt": "Configurações do adaptador para frontier_silicon",
+		"nl": "Adapterinstellingen voor frontier_silicon",
+		"fr": "Paramètres d'adaptateur pour frontier_silicon",
+		"it": "Impostazioni dell'adattatore per frontier_silicon",
+		"es": "Ajustes del adaptador para frontier_silicon",
+		"pl": "Ustawienia adaptera dla frontier_silicon",
+		"zh-cn": "frontier_silicon的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
